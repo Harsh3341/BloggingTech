1f85a7f91ce233c83b46fcfd089ec28f87df20d5
Harsh <harshgupta3341@gmail.com>
2022-10-09 22:58:05 +0530

    minor change

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,10 +2,10 @@ const SignUp = () => {
   return (
     <>
       <div className="bg-gray-100 h-screen flex justify-center items-center p-8">
-        <div className="shadow-sm border-2 border-y-stone-900 bg-gray-100 h-5/6 w-2/3 transition  ease-in-out delay-150 hover:-translate-y-1 hover:shadow-2xl duration-300 flex flex-col items-center p-8">
+        <div className="shadow-sm border-y-2 border-y-stone-900 bg-gray-100 h-5/6 w-2/3 transition  ease-in-out delay-150 hover:-translate-y-1 hover:shadow-2xl duration-300 flex flex-col items-center p-8">
           <h1>Sign Up</h1>
           <div className="flex justify-center items-center w-full h-full">
-            <form className="border-2 border-gray-500 h-3/4 w-3/4 p-8">
+            <form className=" h-3/4 w-3/4 p-8">
               <div className="flex flex-col items-center">
                 <label>Username</label>
                 <input className="border-2 border-gray-500 h-10" type="email" />
