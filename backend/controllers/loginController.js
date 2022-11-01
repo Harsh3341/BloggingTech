28c9b75faee0aeea008eb7d153995d5366bca06c
Harsh <harshgupta3341@gmail.com>
2022-11-01 10:16:10 +0530

    worked not reset password feature

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -3,6 +3,8 @@ const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -35,6 +37,86 @@ const logoutUser = asyncHandler(async (req, res) => {
   });
 });
 
+// Forgot password
+const forgotPassword = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // Get Password Token
+  const resetToken = user.getresetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is as follow: \n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Blogger Password Recovery",
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(500);
+    throw new Error("Email could not be sent");
+  }
+});
+
+// Reset password
+const resetPassword = asyncHandler(async (req, res) => {
+  // Hash token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  // find user in db
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Password reset token is invalid or has expired");
+  }
+
+  if (req.body.password != req.body.confirmPassword) {
+    res.status(400);
+    throw new Error("Passowrd does not match");
+  }
+
+  // set new password
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  user.password = hashedPassword;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(generateToken(user._id), 200, res, user);
+});
+
 // Generate JWT token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -42,4 +124,4 @@ const generateToken = (id) => {
   });
 };
 
-module.exports = { loginUser, logoutUser };
+module.exports = { loginUser, logoutUser, forgotPassword, resetPassword };
