7d9678d75fadbe60335444fcb620171f1d5963ba
Harsh <harshgupta3341@gmail.com>
2022-12-03 21:03:06 +0530

    minor fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -26,7 +26,9 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(...middleware))
+    : applyMiddleware(...middleware)
 );
 
 export default store;
